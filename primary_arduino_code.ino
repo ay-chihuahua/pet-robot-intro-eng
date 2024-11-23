68ca863a555b60be53cc8b1ecce654b19d2915b3
ay-chihuahua <115854680+ay-chihuahua@users.noreply.github.com>
2024-11-22 19:41:07 -0500

    Cleaning up more

--- primary_arduino_code.ino
+++ primary_arduino_code.ino
@@ -1,22 +1,19 @@
 #include <ArduinoBLE.h>
 
-BLEService commandService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Custom service UUID
-BLEStringCharacteristic commandCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 10); // Command characteristic
+BLEService commandService("19B10000-E8F2-537E-4F6C-D104768A1214");
+BLEStringCharacteristic commandCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 10);
 
 void setup() {
     Serial.begin(9600);
     while (!Serial);
-
-    // Initialize Serial1 for communication with the Secondary Arduino
     Serial1.begin(9600);
 
-    BLE.setLocalName("PrimaryArduino");
+    BLE.setLocalName("Arduino");
     BLE.setAdvertisedService(commandService);
     commandService.addCharacteristic(commandCharacteristic);
     BLE.addService(commandService);
 
     BLE.advertise();
-    Serial.println("The primary Arduino ");
 }
 
 void loop() {
@@ -24,11 +21,11 @@ void loop() {
     BLEDevice central = BLE.central();
 
     if (central) {
-        Serial.print("Connected to the app!"); // runs if device is connected
+        Serial.print("Connected to the app!");
         Serial.println(central.address());
 
         while (central.connected()) {
-            if (commandCharacteristic.written()) { // comand read from app
+            if (commandCharacteristic.written()) {
 
                 String command = commandCharacteristic.value();
                 Serial.print("Received command via Bluetooth: ");
