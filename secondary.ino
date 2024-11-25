fc11e83cc8900823061810a585f83d8d7bb15112
ay-chihuahua <115854680+ay-chihuahua@users.noreply.github.com>
2024-11-25 14:24:28 -0500

    Added switch statement and commented out old code

--- secondary.ino
+++ secondary.ino
@@ -15,31 +15,45 @@ void setup() {
 /* TO-DO:
   Write a separate function for trimming
   Maybe write seperate functions for each command
+  Remove commented out code if what is written works
 */
 void loop() {
-    if (Serial.available() > 0) {
+    string command = ""; // might be redundant
+    
+    if (Serial.available() > 0) { // checks if the command is received from the primary Arduino
         /*String command = Serial.readStringUntil('\n');
         command.trim();
         Serial.print("Received command from Primary Arduino: ");
         Serial.println(command);*/
 
-        trimCommand(command);
+        trimCommand(command); // function to make command match switch statement
 
+        switch(trimmedCommand){ // depending on what the command says, run specified state
+            case "toggle"; 
+                digitalWrite(LED_PIN, !digitalRead(LED_PIN)); // example case for testing.
+                Serial.println("LED state toggled!"); // consider making function so code is cleaner
+                break;
+            default:
+                Serial.println("Error, command not accepted");
+                break;
+        }
+        /*
         if (command == "toggle") {
             digitalWrite(LED_PIN, !digitalRead(LED_PIN)); 
             Serial.println("LED state toggled on Secondary Arduino.");
         } else {
             Serial.println("Unknown command received.");
         }
+        */
     }
 }
 
 // /*
-string trimCommand(command){
+string trimCommand(command){ // trims command so no whitespace or new lines exist
         String command = Serial.readStringUntil('\n');
         command.trim();
         Serial.print("Received command from Primary Arduino: ");
         Serial.println(command);
-    return command;
+    return trimmedCommand;
 }
 // */
